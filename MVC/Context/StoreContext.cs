0d2ff5b1049745a8d7a28148bdfed65f342181a3
juan <vilfrack21@gmail.com>
2017-02-06 20:29:53 -0400

    Parte numero 03 del curso mvc. Se se crea el objecto Product y la opcion de agregar registros

---
+++ MVC/Context/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Context
+{
+    public class StoreContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MVC/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastBuy { get; set; }
+        public float Stock { get; set; }
+    }
+}
